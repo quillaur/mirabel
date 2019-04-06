64c84b540423108a77b8446f0894b7b5711f2df4
Quillet <aurelienquillet@gmail.com>
2019-04-06 16:19:20 +0200

    Externalize download function.

--- scripts/update_targetscan_data.py
+++ scripts/update_targetscan_data.py
@@ -4,48 +4,12 @@
 # Purpose: Get raw data from targetscan website
 
 import logging
-from requests import get  # to make GET request
-import os.path
 import sys
 import click
 import zipfile
 
 # Personal imports
 import utilities
-
-
-def download(url: str, file_name: str):
-    """
-    Download file from URL.
-    :param url: address to get the file from.
-    :param file_name: name of file to write downloaded data in.
-    :return: None
-    """
-    # Check that file was not already downloaded
-    download_tag = True
-    short_file_name = file_name.split("/")[-1]
-    if os.path.exists(file_name):
-        # If so, do you wish to download it anyway?
-        if not click.confirm("{} already exists on your system. "
-                             "Do you still wish to download it anyway?".format(short_file_name), default=False):
-            download_tag = False
-
-    if download_tag:
-        logging.info("Downloading {}...".format(url))
-        # open in binary mode
-        with open(file_name, "wb") as file:
-            # get request
-            response = get(url)
-            # write to file
-            file.write(response.content)
-
-        # Check that download is successful:
-        if os.path.exists(file_name):
-            logging.info("Download successful.")
-        else:
-            logging.warning("Download failed.")
-    else:
-        logging.info("You do not wish to download {}".format(short_file_name))
 
 
 def read_txt_zip_file(filename: str, species: str):
@@ -108,7 +72,7 @@ if __name__ == '__main__':
 
     # Download
     try:
-        download(url, file_name)
+        utilities.download(url, file_name)
 
     except Exception as e:
         logging.error("Download issue: {}".format(e))
--- scripts/utilities.py
+++ scripts/utilities.py
@@ -6,6 +6,12 @@
 import configparser
 import os
 import mysql.connector
+import click
+import logging
+from requests import get  # to make GET request
+
+# Set logging module
+logging.basicConfig(level="DEBUG", format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 def extract_config():
@@ -51,3 +57,36 @@ def chunk(input_list: list, chunk_size: int):
     for i in range(0, len(input_list), chunk_size):
         yield input_list[i:i + chunk_size]
 
+
+def download(url: str, file_name: str):
+    """
+    Download file from URL.
+    :param url: address to get the file from.
+    :param file_name: name of file to write downloaded data in.
+    :return: None
+    """
+    # Check that file was not already downloaded
+    download_tag = True
+    short_file_name = file_name.split("/")[-1]
+    if os.path.exists(file_name):
+        # If so, do you wish to download it anyway?
+        if not click.confirm("{} already exists on your system. "
+                             "Do you still wish to download it anyway?".format(short_file_name), default=False):
+            download_tag = False
+
+    if download_tag:
+        logging.info("Downloading {}...".format(url))
+        # open in binary mode
+        with open(file_name, "wb") as file:
+            # get request
+            response = get(url)
+            # write to file
+            file.write(response.content)
+
+        # Check that download is successful:
+        if os.path.exists(file_name):
+            logging.info("Download successful.")
+        else:
+            logging.warning("Download failed.")
+    else:
+        logging.info("You do not wish to download {}".format(short_file_name))
