1b5cda11bc8fdded801bd3a2e9eb6201c1f5cd6b
Quillet <aurelienquillet@gmail.com>
2019-06-13 07:23:16 +0200

    Minor fix.

--- scripts/aggregation.r
+++ scripts/aggregation.r
@@ -3,12 +3,12 @@ InsPack = function(pack)
 { 
 	if (!pack %in% installed.packages()) 
 	{ 
-		print(paste("installing",pack)) 
+		# print(paste("installing",pack)) 
 		install.packages(pack)
 	} 
 	else
 	{
-		print(paste(pack," already installed"))
+		# print(paste(pack," already installed"))
 	}
 } 
 InsPack("RobustRankAggreg")
