62de2bff4d517e6313192d40269f9a6652564be2
Dubessyadmin <christophe.dubessy@univ-rouen.fr>
2019-07-24 18:23:59 +0200

    Fix from Christophe machine.

--- flask_mirabel.py
+++ flask_mirabel.py
@@ -517,7 +517,9 @@ def get_existing_comparisons():
         del db_compared[-1]
         if len(db_compared) > 1:
             db_compared = " and ".join(db_compared)
-        comparisons.append("{} vs {} and all in one = {}".format(db_list[0], db_compared, all_in_one))
+            comparisons.append("{} vs {} and all in one = {}".format(db_list[0], db_compared, all_in_one))
+        else:
+            comparisons.append("{} vs {} and all in one = {}".format(db_list[0], db_compared[0], all_in_one))
 
     return comparisons
 
